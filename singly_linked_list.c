34871ca6b0f483be0c56a342e91d588ba30fb857
Saad Mazhar <saadmazharr@gmail.com>
2024-11-12 16:34:06 -0500

    Add files via upload

---
+++ singly_linked_list.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int val;
+    struct Node* next;
+};
+
+void print_nodes(struct Node* head){
+    printf("Node Values:");
+    while (head != NULL){
+        printf(" %d", head->val);
+        head = head->next;
+    }
+    printf("\n");
+    return;
+}
+
+struct Node* make_node(int value){
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node*));
+
+    if (new_node == NULL){
+        puts("malloc returned null");
+        return NULL;
+    }
+
+    new_node->val = value;
+    new_node->next = NULL;
+
+    return new_node;
+
+}
+
+void insert_node(struct Node** head, int value){
+    struct Node* new_node = make_node(value);
+
+    if (new_node == NULL) {
+        puts("new node creation failed");
+        return;
+    }
+
+    struct Node* last_node = *head;
+
+    if (*head == NULL){
+        *head = new_node;
+        return;
+    }
+
+    while (last_node->next != NULL){
+        last_node = last_node->next;
+    }
+
+    last_node->next = new_node;
+    
+
+    return;
+}
+
+int main(){
+    struct Node* head = NULL;
+
+    insert_node(&head, 1);
+    insert_node(&head, 2);
+    insert_node(&head, 3);
+    insert_node(&head, 4);
+    insert_node(&head, 5);
+
+    print_nodes(head);
+
+    return 0;
+}
